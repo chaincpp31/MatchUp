702c8120f1f04ee49a8aa567fe905e69fe994992
jeffyso <jeffyzaja@gmail.com>
2020-09-10 16:12:09 +0700

    update-indexFunc-gamerController

--- app/Controllers/Http/GamerController.js
+++ app/Controllers/Http/GamerController.js
@@ -1,6 +1,16 @@
 'use strict'
-
+const Database = use('Database')
+const Gamer = use('App/Models/Gamer')
+const GamerUtil = use('../../../util/gamerjectUtil')
 class GamerController {
+    async index({ request }){
+        // subjects?references=teachers
+        const { references } = request.qs
+        console.log(references)
+        const gamerUtil = new GamerUtil()
+        const gamers = await gamerUtil.getAll(references)
+        return { status:200,error:undefined,data:await gamers.fetch() }
+    }
 }
 
 module.exports = GamerController
