3ef355f5039b877396d38e980b4d757f2afb565c
chaincpp31 <chaincpp31@gmail.com>
2020-09-13 02:59:08 +0700

    add gamer hash password

--- app/Models/Gamer.js
+++ app/Models/Gamer.js
@@ -2,8 +2,18 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const Hash = use('Hash')
 
 class Gamer extends Model {
+    static boot() {
+        super.boot()
+
+        this.addHook('beforeSave', async (gamerInstance) => {
+            if (gamerInstance.dirty.password) {
+                gamerInstance.password = await Hash.make(gamerInstance.dirty.password)
+            }
+        })
+    }
     static get primaryKey(){
         return 'gamer_id' //เพื่อไม่ให้ defaultเป็นชื่อ 'id'
     }
