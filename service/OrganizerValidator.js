129b8ebf16df634adad6e1941aa5a0857dd9ce9b
jeffyso <jeffyzaja@gmail.com>
2020-09-13 20:45:12 +0700

    update-service-OrganizerValidator

---
+++ service/OrganizerValidator.js
@@ -0,0 +1,27 @@
+const Validator = use("Validator")
+
+module.exports = async function organizerValidator (data) {
+  if (typeof data !== 'object') throw new Error()
+
+  const { name,user_name,password,first_name,last_name,email,phone_number,age,birth_day } = data
+
+  const rules = {
+    name: 'required|unique:gamers,name',
+    user_name: 'required',
+    password: 'required|min:8',
+    first_name: 'required',
+    last_name: 'required',
+    email: 'required|email|unique:teachers,email',
+    phone_number: 'required',
+    age: 'required',
+    birth_day: 'required'
+  }
+
+  const validation = await Validator.validateAll({
+    name,user_name,password,first_name,last_name,email,phone_number,age,birth_day
+  }, rules)
+
+  return {
+    error: validation.messages()
+  }
+}
