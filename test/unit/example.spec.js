c9377684df92d1fa8b2c63720bee3608472b7956
jeffyso <jeffyzaja@gmail.com>
2020-10-22 14:37:04 +0700

    don't-use-example-spec

--- test/unit/example.spec.js
+++ test/unit/example.spec.js
@@ -1,7 +1,17 @@
 'use strict'
-
+const GamerValidator = require('../../service/GamerValidator')
 const { test } = use('Test/Suite')('Example')
 
 test('make sure 2 + 2 is 4', async ({ assert }) => {
   assert.equal(2 + 2, 4)
+})
+
+test('should return only one error if single incorrect data is passed',async({ assert })=>{
+  const validatedData = await GamerValidator({
+      first_name:"John",
+      last_name:"Doe",
+      email:"john@email.com",
+      password:"1234567"})
+  assert.equal(validatedData.error.length,9)
+
 })
