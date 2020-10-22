fa0729e02b301c0167074c398c2c873dc6eb5514
jeffyso <jeffyzaja@gmail.com>
2020-10-22 14:35:29 +0700

    edit-gamer-validator

--- service/GamerValidator.js
+++ service/GamerValidator.js
@@ -7,18 +7,18 @@ module.exports = async function gamerValidator (data) {
 
   const rules = {
     name: 'required|unique:gamers,name',
-    user_name: 'required',
+    user_name: 'required|unique',
     password: 'required|min:8',
     first_name: 'required',
     last_name: 'required',
-    email: 'required|email|unique:teachers,email',
+    email: 'required|email|unique',
     phone_number: 'required',
     age: 'required',
     birth_day: 'required'
   }
 
   const validation = await Validator.validateAll({
-    name,user_name,password,first_name,last_name,email,phone_number,age,birth_day
+    name,user_name,password,email,first_name,last_name,phone_number,age,birth_day
   }, rules)
 
   return {
