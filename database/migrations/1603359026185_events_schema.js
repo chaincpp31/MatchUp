28c92cfd5a744ce95a22627f96a5c4f746ababc6
jeffyso <jeffyzaja@gmail.com>
2020-10-24 00:06:05 +0700

    add-events-migration

---
+++ database/migrations/1603359026185_events_schema.js
@@ -0,0 +1,29 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class EventsSchema extends Schema {
+  up () {
+    this.create('events', (table) => {
+      table.increments('events_id')
+      table.string('name_event').notNullable()
+      table.integer("clients_id").notNullable().unsigned()
+      table.timestamp('date_events').notNullable().unique()
+
+      table.foreign('clients_id')
+        .references('clients.clients_id')
+        .onDelete('CASCADE') // ON DELETE CASCADE
+        .onUpdate('CASCADE') // ON UPDATE CASCADE
+
+
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('events')
+  }
+}
+
+module.exports = EventsSchema
