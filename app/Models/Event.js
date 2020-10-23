a2da89775786ff9593c516941ba6389c8dfebcac
jeffyso <jeffyzaja@gmail.com>
2020-10-24 00:02:05 +0700

    update-EventModel

--- app/Models/Event.js
+++ app/Models/Event.js
@@ -11,13 +11,17 @@ class Event extends Model {
     static get createdAtColumn(){
         return null ;
     }
-    
+
     static get updatedAtColumn(){
         return null ;
     }
 
-    gamers(){
-        return this.hasMany('App/Models/Gamer')
+    eventGamer(){
+      return this.hasMany('App/Models/Events')
+    }
+
+    clients(){
+        return this.belongsToMany('App/Models/Clients')
     }
     // organizers(){
     //     return this.belongsToMany('App/Models/Organizer')
